5522ce88841a2e515824f34915fc17c0e7a6b4ab
med.wf <med.wf95@gmail.com>
2024-05-06 17:18:06 +0100

    add task 1 - pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,6 +4,7 @@ import csv
 import math
 from typing import List, Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end indexes for pagination.
