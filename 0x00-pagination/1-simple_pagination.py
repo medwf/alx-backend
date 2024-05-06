a245af065b01c4bff60d6c7ac3c146b946bffc40
med.wf <med.wf95@gmail.com>
2024-05-06 16:56:56 +0100

    add task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate the start and end indexes for pagination.
+    Args:
+        page <int>: the current page number (1-indexed).
+        page_size <int>: The number of items per page.
+    return:
+        <Tuple[int, int]> of size two containing
+            start index and an end index
+    """
+    return (
+        (page - 1) * page_size,
+        ((page - 1) * page_size) + page_size
+        )
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                # print("#")
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get page using simple pagination.
+
+        Args:
+            page <int> 1 as default: the current page number (1-indexed).
+            page_size <int> 10 as default: The number of items per page.
+
+        return:
+            <list[list]> list data in list
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+        return data[start:end] if len(data) > 0 else []
