f06505f1e53952774ca32a8ae9bc6f4cdbe75dac
med.wf <med.wf95@gmail.com>
2024-05-22 18:57:17 +0100

    add task 4 - MRU it is same as LRU but del from end

--- 0x01-caching/main.py
+++ 0x01-caching/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # for task 0
-""" 0-main """
+""" 0-main 
 BasicCache = __import__('0-basic_cache').BasicCache
 
 my_cache = BasicCache()
@@ -20,3 +20,114 @@ my_cache.put("E", "Battery")
 my_cache.put("A", "Street")
 my_cache.print_cache()
 print(my_cache.get("A"))
+"""
+
+""" 1-main 
+FIFOCache = __import__('1-fifo_cache').FIFOCache
+
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+"""
+
+"""for task 2"""
+""" 2-main 
+LIFOCache = __import__('2-lifo_cache').LIFOCache
+
+my_cache = LIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print('_' * 40)
+
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+print('_' * 40)
+
+
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print('_' * 40)
+
+
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+print('_' * 40)
+
+
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+print('_' * 40)
+"""
+
+""" 3-main 
+LRUCache = __import__('3-lru_cache').LRUCache
+
+my_cache = LRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
+"""
+
+""" 4-main """
+MRUCache = __import__('4-mru_cache').MRUCache
+
+my_cache = MRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
