d7c2919b6ae166329ecaecb8a85ea58b6d295f7e
med.wf <med.wf95@gmail.com>
2024-05-22 12:00:53 +0100

    add task 0 - add class BasicCache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,38 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""BasicCache Module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCaching define.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        put key value to dict cache_data
+        Args:
+            key: the key of item.
+            item: the value that stored.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        get value of some item
+
+        Args:
+            key: the key of item
+
+        return : item or None if dosn't exist.
+        """
+        value = None
+        if key:
+            value = self.cache_data.get(key, None)
+        return value
