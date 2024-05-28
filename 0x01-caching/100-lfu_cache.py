3e75a1ed0669ae118a618d86b23ce13896848f5c
med.wf <med.wf95@gmail.com>
2024-05-28 11:17:09 +0100

    add new projects i18n

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,1 +1,49 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+LFUCache  Module
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LFUCache(BaseCaching):
+    """
+    LFUCache define
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        put a key value to a cache data
+
+        Args:
+            key: the key of item.
+            item: the item that should stored in cache data.
+        """
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                Key = list(self.cache_data)[0]
+                del self.cache_data[Key]
+                print(f"DISCARD: {Key}")
+
+    def get(self, key):
+        """
+        get item using key
+
+        Args:
+            key: the key of item
+
+        return: item or None if not exist
+        """
+        value = None
+        if key:
+            value = self.cache_data.get(key, None)
+            if value:
+                self.cache_data.move_to_end(key)
+        return value
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## 0x00. Pagination
 ## 0x01. caching
+## 0x02. i18n
