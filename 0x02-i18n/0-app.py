7977ac52b44043cd268457658a551571ea3dfcef
med.wf <med.wf95@gmail.com>
2024-05-28 12:47:14 +0100

    add task 0 - delete pass parameters

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,14 +8,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def home() -> str:
     """The route of flask app"""
-    return render_template(
-        "0-index.html",
-        tittle="Welcome to Holberton",
-        h1="Hello world"
-    )
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
