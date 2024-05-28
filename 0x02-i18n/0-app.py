bd98f58e55cb34d6755bd07ce01ec64842cc64dd
med.wf <med.wf95@gmail.com>
2024-05-28 12:21:22 +0100

    add task 0 - annotations, set host and port

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def home() -> str:
     """The route of flask app"""
     return render_template(
         "0-index.html",
@@ -19,4 +19,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
