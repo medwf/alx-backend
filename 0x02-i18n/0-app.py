15ce712f23bab16a9aecf9602e49f5b425b23261
med.wf <med.wf95@gmail.com>
2024-05-28 12:15:13 +0100

    add task 0 - basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+setup a basic Flask app 
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """The route of flask app"""
+    return render_template(
+        "0-index.html",
+        tittle="Welcome to Holberton",
+        h1="Hello world"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
