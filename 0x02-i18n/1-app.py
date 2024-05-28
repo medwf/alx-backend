37c520d584b0ddbcb8b6020c9eee30751ec7f869
med.wf <med.wf95@gmail.com>
2024-05-28 13:29:55 +0100

    add task 1 - start config babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+setup a basic Flask app
+"""
+
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCAL = "en"
+    TIME_ZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.route("/")
+def home() -> str:
+    """The route of flask app"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
