436d139a0ed8c316a05f9b8900569c2fe2e681e3
med.wf <med.wf95@gmail.com>
2024-05-29 14:27:23 +0100

    add task 5 - login, 4

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""A Basic Flask app with internationalization support.
 """
-from flask_babel import Babel, _
-from flask import Flask, render_template, request, g
+from flask_babel import Babel
 from typing import Union, Dict
+from flask import Flask, render_template, request, g
 
 
 class Config:
@@ -18,13 +18,29 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
-
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
@@ -35,20 +51,6 @@ def get_locale() -> str:
     if locale in app.config["LANGUAGES"]:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-def get_user() -> Union[Dict, None]:
-    """methods that return user dict"""
-    login_id = request.args.get('login_as')
-    if login_id:
-        return users.get(int(login_id))
-    return None
-
-
-@app.before_request
-def before_request() -> None:
-    """this function set user to global on g.user"""
-    g.user = get_user()
 
 
 @app.route('/')
